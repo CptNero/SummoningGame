8a9c9fb9d6601cc09f9eb4410d772ca0827236c5
talanhuszk <bugacy713@gmail.com>
2024-04-14 20:09:40 +0200

    Added Visuals

---
+++ Assets/Scripts/UI Effects/WobblyText.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class WobblyText : MonoBehaviour
+{
+   public TMP_Text textComponent;
+    // Update is called once per frame
+    void Update()
+    {
+        textComponent.ForceMeshUpdate();
+        var textInfo = textComponent.textInfo;
+
+        for(int i = 0; i < textInfo.characterCount-1; i++)
+        {
+            var charinfo = textInfo.characterInfo[i];
+            if(!charinfo.isVisible)
+            {
+                continue;
+            }
+
+            var verts = textInfo.meshInfo[charinfo.materialReferenceIndex].vertices;
+
+            for(int j = 0; j < 4; j++)
+            {
+                var orig = verts[charinfo.vertexIndex + j];
+                verts[charinfo.vertexIndex + j] = orig + new Vector3(1,Mathf.Sin(Time.time*2 + orig.x * 0.1f) * 10, 0);
+            }
+        }
+
+        for(int i = 0; i < textInfo.meshInfo.Length; i++)
+        {
+            var meshInfo = textInfo.meshInfo[i];
+            meshInfo.mesh.vertices = meshInfo.vertices;
+            textComponent.UpdateGeometry(meshInfo.mesh,i);
+        }
+    }
+}
